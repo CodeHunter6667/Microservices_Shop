8ea6990ee7cbf07a7abc816dae7c9b85507a4739
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-08 14:39:49 -0300

    feat: adicionado configurações e conexão com o vanco de dados

---
+++ MicroservicesShop.ProductApi/Commom/BuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using MicroservicesShop.ProductApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroservicesShop.ProductApi.Commom;
+
+public static class BuilderExtensions
+{
+    public static void AddConfigurations(this WebApplicationBuilder builder)
+    {
+        Configuration.ConnectionString = builder.Configuration.GetConnectionString("ProductDbConnection") ?? string.Empty;
+    }
+
+    public static void AddDbContexts(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ProductDbContext>(options =>
+        {
+            options.UseSqlServer(Configuration.ConnectionString);
+        });
+    }
+}
---
+++ MicroservicesShop.ProductApi/Configuration.cs
@@ -0,0 +1,6 @@
+﻿namespace MicroservicesShop.ProductApi;
+
+public static class Configuration
+{
+    public static string ConnectionString { get; set; } = string.Empty;
+}
