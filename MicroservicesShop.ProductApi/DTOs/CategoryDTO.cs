89810f85901fa158d039c554bd18dc39a3150076
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-08 15:41:41 -0300

    feat: criado DTOs do serviço de Produtos

---
+++ MicroservicesShop.ProductApi/DTOs/CategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using MicroservicesShop.ProductApi.Models;
+
+namespace MicroservicesShop.ProductApi.DTOs;
+
+public class CategoryDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<Product> Products { get; set; } = [];
+}
