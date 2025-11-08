d01beba004f7d2ccd8077d58cb8dd6d3370cdfbb
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-08 15:42:05 -0300

    feat: criado extensões dos Models

---
+++ MicroservicesShop.ProductApi/Extensions/CategoryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using MicroservicesShop.ProductApi.DTOs;
+using MicroservicesShop.ProductApi.Models;
+
+namespace MicroservicesShop.ProductApi.Extensions;
+
+public static class CategoryExtensions
+{
+    public static Category MapToCategory(this CategoryDTO dto)
+    {
+        return new Category
+        {
+            Id = dto.Id,
+            Name = dto.Name,
+            Products = dto.Products
+        };
+    }
+
+    public static CategoryDTO MapToDTO(this Category category)
+    {
+        return new CategoryDTO
+        {
+            Id = category.Id,
+            Name = category.Name,
+            Products = category.Products
+        };
+    }
+}
---
+++ MicroservicesShop.ProductApi/Extensions/ProductExtensions.cs
@@ -0,0 +1,35 @@
+﻿using MicroservicesShop.ProductApi.DTOs;
+using MicroservicesShop.ProductApi.Models;
+
+namespace MicroservicesShop.ProductApi.Extensions;
+
+public static class ProductExtensions
+{
+    public static Product MaPToProduct(this ProductDTO dto)
+    {
+        return new Product
+        {
+            Id = dto.Id,
+            Name = dto.Name,
+            Price = dto.Price,
+            Description = dto.Description,
+            Stock = dto.Stock,
+            ImageUrl = dto.ImageUrl,
+            CategoryId = dto.CategoryId
+        };
+    }
+
+    public static ProductDTO MapToDTO(this Product product)
+    {
+        return new ProductDTO
+        {
+            Id = product.Id,
+            Name = product.Name,
+            Price = product.Price,
+            Description = product.Description,
+            Stock = product.Stock,
+            ImageUrl = product.ImageUrl,
+            CategoryId = product.CategoryId
+        };
+    }
+}
